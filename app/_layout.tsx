df2469b3723d1150c2efb68c7fdcf49a273ba22c
Martin Kopecký <kopecky@thekrew.app>
2024-03-11 11:15:37 +0100

    base for coms ready

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,11 +13,9 @@ export {
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -50,8 +48,7 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name='index'/>
       </Stack>
     </ThemeProvider>
   );
